d8d432c52c7db2272be02f1ab6ef8fa96a880dcf
pavithra <pavithradhinakaran26@gmail.com>
2024-11-07 15:57:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function myfunc(event){
+    event.preventDefault();
+}
+var full_name = document.getElementById("full_name").Value;
+var age = document.getElementById("age").Value;
+var city = document.getElementById("city").Value;
+
+
+
+ 
+
+
+
+
+
+
+
